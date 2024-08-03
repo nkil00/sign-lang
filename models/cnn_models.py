1823da74a851bbaf3ee4545982a5850d5399eb45
Lino <lino.r.knote@gmail.com>
2024-08-03 18:11:40 +0200

    Adjusted variable names

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -7,19 +7,19 @@ num_classes = 36
 class ConvSignLangNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv2 = nn.Conv2d(3, 16, 3)
-        self.pool = nn.MaxPool3d(2, 2)
-        self.conv3 = nn.Conv2d(16, 8, 5)
-        self.fc1 = nn.Linear(8 * 29 * 29, 256)
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 8, 5)
+        self.fc1 = nn.Linear(8*29*29, 256)
         self.fc2 = nn.Linear(256, 120)
         self.fc3 = nn.Linear(120, 84)
         self.fc4 = nn.Linear(84, num_classes)
     
     def forward(self, x):
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x))) # ([32, 8, 29, 29])
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x))) # ([32, 8, 29, 29])
         # fully connected layers 
-        x = torch.flatten(x, 2) # flatten all dimensions except batch
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
