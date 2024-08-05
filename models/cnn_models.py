cd66cba321f220f577008f68defe7ec5a9579724
nkil00 <lino.r.knote@gmail.com>
2024-08-05 14:13:06 +0200

    added new model, changed actv of last layer

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-num_classes = 36
+_num_classes = 36
 
 class ConvSignLangNN(nn.Module):
     def __init__(self):
@@ -13,7 +13,7 @@ class ConvSignLangNN(nn.Module):
         self.fc1 = nn.Linear(8*29*29, 256)
         self.fc2 = nn.Linear(256, 120)
         self.fc3 = nn.Linear(120, 84)
-        self.fc4 = nn.Linear(84, num_classes)
+        self.fc4 = nn.Linear(84, _num_classes)
     
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -25,4 +25,29 @@ class ConvSignLangNN(nn.Module):
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
         
+        return x
+
+class ConvSignLangNN_6(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 8, 5)
+        self.fc1 = nn.Linear(8*29*29, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 84)
+        self.fc5 = nn.Linear(84, _num_classes)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x))) # ([32, 8, 29, 29])
+        # fully connected layers 
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)       
+
         return x
