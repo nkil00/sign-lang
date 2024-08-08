6b3a4786393eb284c2e6502978222a161819719b
nkil00 <lino.r.knote@gmail.com>
2024-08-08 11:38:37 +0200

    added model

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -27,6 +27,34 @@ class ConvSignLangNN(nn.Module):
         
         return x
 
+class ConvSignLangNN_7(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 8, 5)
+        self.fc1 = nn.Linear(8*29*29, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 128)
+        self.fc5 = nn.Linear(128, 84)
+        self.fc6 = nn.Linear(84, _num_classes)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x))) # ([32, 8, 29, 29])
+        # fully connected layers 
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc6(x)       
+        return x
+
+
+
 class ConvSignLangNN_6(nn.Module):
     def __init__(self):
         super().__init__()
