b8b930cd3a0326f7a5b612cbbbc0112bd365b94d
nkil00 <lino.r.knote@gmail.com>
2024-08-19 11:32:05 +0200

    added grid-search demo

--- sign-lang-train.py
+++ sign-lang-train.py
@@ -8,22 +8,48 @@ import os
 LABEL_PATH = os.path.join(".", "data", "sign_lang_train", "labels.csv") 
 IMG_DIR = os.path.join(".", "data", "sign_lang_train")
 "./models/state_dicts/"
-MODEL_DIR = os.path.join(".", "models", "state_dicts")
+MODEL_DIR = os.path.join(".", "eval", "model-prm", "grids")
+INFO_DIR = os.path.join(".", "eval", "info", "grids")
 
+EPOCHS = 15
+BATCH_SIZE = 32
+
+
+
+def grid_lr(grid_params: dict):
+    lrs = grid_params["lr"]
+    batch_size = grid_params["batch_size"]
+    epochs = grid_params["epochs"]
+
+    for epoch in epochs:
+        for bs in batch_size:
+            for lr in lrs:
+                print("Epochs: ", epoch)
+                print("BatchSize: ", bs)
+                print("Learning Rate:", lr)
+                tsm = TrainSignLang(epochs=epoch,
+                                      lr=lr,
+                                      train_set_size=0.8,
+                                      batch_size=bs)
+                tsm.init_data(image_dir=IMG_DIR,
+                                labels_path=LABEL_PATH)
+
+                model = ConvSignLangNN_7()
+                lossf = CrossEntropyLoss()
+                tsm.init_model(model=model,
+                               loss_fn=lossf,
+                               optim="Adam")
+                tsm.train()
+                tsm.evaluate()
+                tsm.save_model(MODEL_DIR)
+                tsm.save_info(INFO_DIR)
+    
 
 if __name__ == "__main__":
-    training_model = TrainSignLang(epochs=1, 
-                                   train_set_size=0.8)
-    training_model.init_data(image_dir=IMG_DIR, labels_path=LABEL_PATH)
-
-    convs7 = ConvSignLangNN_7()
-    optim = "Adam"
-    loss_fn = CrossEntropyLoss()
-    training_model.init_model(model=convs7, loss_fn=loss_fn, optim=optim)
-
-    print(training_model)
-
-    training_model.train()
-    ac, loss = training_model.evaluate()
-    print(f"Accuracy = {ac:.3f}")
-    training_model.save_model(MODEL_DIR, vocal=True)
+    lrs = [0.001, 0.005, 0.0001]
+    grid_params = {
+        "lr": lrs,
+        "batch_size": [32, 64, 256],
+        "epochs": [10, 15]
+    }
+    grid_lr(grid_params)
