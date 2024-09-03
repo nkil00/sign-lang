9c58d979cd8be47d0bf97046b1b775cf4155fddf
nkil00 <lino.r.knote@gmail.com>
2024-09-03 22:00:39 +0200

    updated params

--- sign-lang-train.py
+++ sign-lang-train.py
@@ -89,8 +89,8 @@ if __name__ == "__main__":
 
     grid_params = {
         "lr": [0.001, 0.0001],
-        "batch_size": [32, 64],
-        "epochs": [10, 15, 20],
+        "batch_size": [32],
+        "epochs": [10, 20],
         "thresholds" : [-1],
         "augment" : [True, False]
     }
